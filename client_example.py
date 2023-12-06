b1a156739813c0c146fd0c8f52f12cc7aa508056
Noa Mills <noa.mills@usda.gov>
2023-12-06 13:53:05 -0800

    implemented upload_geometry() function

--- client_example.py
+++ client_example.py
@@ -1,10 +1,13 @@
 import sys
 import pygcdl as pygcdl
 import geopandas as gpd
+import os
 
 pygcdl_obj = pygcdl.PyGeoCDL()
-#print(pygcdl_obj.getDatasets())
-#bprint(pygcdl_obj.getDatasetInfo("MODIS_NDVI"))
+print(pygcdl_obj.getDatasets())
+bprint(pygcdl_obj.getDatasetInfo("MODIS_NDVI"))
 
 #Sample geometry stored as clay_aoi.geojson, square polygon in Clay county, Iowa.
-pygcdl_obj.uploadGeom("clay_aoi.geojson")
+pygcdl_obj.upload_geometry("clay_aoi.geojson")
+pygcdl_obj.upload_geometry("jer_bounds_sf.shp")
+pygcdl_obj.upload_geometry("jer_bounds_sf.zip")
--- pygcdl.py
+++ pygcdl.py
@@ -1,11 +1,14 @@
 import requests
+import os
+from pathlib import Path
+import shutil
 
 class PyGeoCDL:
 	def __init__(self, url_base=None):
 		if url_base is None:
 			self.url_base='http://127.0.0.1:8000'
 
-	def getDatasets(self):
+	def list_datasets(self):
 		"""
 		Retrieves the ID and name of all available GeoCDL datasets. The
 		dataset information is returned as a dictionary in which the dataset
@@ -15,7 +18,7 @@ class PyGeoCDL:
 
 		return {val['id']: val['name'] for val in r.json()}
 
-	def getDatasetInfo(self, dsid):
+	def view_metadata(self, dsid):
 		"""
 		Returns all metadata for the dataset with the given dataset ID. The
 		metadata are returned as a dictionary of key: value pairs.
@@ -25,9 +28,54 @@ class PyGeoCDL:
 
 		return r.json()
 
-	def uploadGeom(self, file):
-		headers = {"Content-Type": "multipart/form-data", "accept": "application/json"}
-		files = {"file": (file, open(file, 'rb'), 'application/geo+json', {'Expires': '0'})}
-		r = requests.post(self.url_base + '/upload_geom', headers = headers, files=files)
-		print(r.text)
-		return r.text
+	def upload_geometry(self, file):
+		if not os.path.isfile(file):
+			raise Exception("File not found")
+		file_ext = os.path.splitext(file)[1]
+		if file_ext == ".geojson" or file_ext == ".zip" or file_ext == ".csv":
+			files = {"geom_file": (file, open(file, 'rb'))}
+			#headers = {'Content-type': 'multipart/form-data; charset=UTF-8'}
+			r = requests.post(self.url_base + '/upload_geom', files=files)
+			print(r.text)
+			return r.text
+		elif file_ext == ".shp":
+			#grab auxiliary files: dbf, shx, prj
+			shp_file_path = os.path.abspath(file) #ie /dir/to/file_name.shp
+			base_data_dir = os.path.dirname(os.path.abspath(file))
+			base_file_path = os.path.splitext(shp_file_path)[0] #ie /dir/to/file_name
+			base_file_name = os.path.splitext(os.path.basename(shp_file_path))[0] #ie file_name
+			dbf_file_path = base_file_path + ".dbf"
+			if not os.path.isfile(dbf_file_path):
+				raise Exception("Insufficient auxiliary shapefile files")
+			shx_file_path = base_file_path + ".shx"
+			if not os.path.isfile(shp_file_path):
+				raise Exception("Insufficient auxiliary shapefile files")
+			prj_file_path = base_file_path + ".prj"
+			if not os.path.isfile(prj_file_path):
+				raise Exception("Insufficient auxiliary shapefile files")
+			#Create a new folder for shapefiles to be zipped in
+			new_data_dir = os.path.abspath(os.path.join(base_data_dir, base_file_name))
+			os.mkdir(new_data_dir)
+			#copy shp file	
+			base_shp_file_name = os.path.basename(file)
+			shutil.copyfile(file, os.path.join(new_data_dir, base_shp_file_name))
+			#copy dbf file
+			base_dbf_file_name = os.path.basename(dbf_file_path)
+			shutil.copyfile(file, os.path.join(new_data_dir, base_dbf_file_name))
+			#copy shx file
+			base_shx_file_name = os.path.basename(shx_file_path)
+			shutil.copyfile(file, os.path.join(new_data_dir, base_shx_file_name))
+			#copy prj file
+			base_prj_file_name = os.path.basename(prj_file_path)
+			shutil.copyfile(file, os.path.join(new_data_dir, base_prj_file_name))
+			shutil.make_archive(base_dir = new_data_dir, root_dir = base_file_path, format = 'zip', base_name = base_file_name)
+			zip_file_name = base_file_name + ".zip"
+			zip_file_path = os.path.join(base_data_dir, zip_file_name)
+			files = {"geom_file": (zip_file_path, open(zip_file_path, 'rb'))}
+			r = requests.post(self.url_base + '/upload_geom', files=files)
+			shutil.rmtree(new_data_dir)
+			print(r.text)
+			return r.text
+		else:
+			raise Exception("File format not yet supported")
+		
