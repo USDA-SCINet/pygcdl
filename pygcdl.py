dfb6311580083098d9b99d92f44a0d19ad6325a4
Noa Mills <noa.mills@usda.gov>
2023-12-28 16:23:49 -0800

    Moved helper functions to main pygcdl class, fixed format_dsvars() to url-encode output

--- pygcdl.py
+++ pygcdl.py
@@ -1,9 +1,11 @@
 import requests
 import os
 from pathlib import Path
-import shutil
-import utils
 import tempfile
+import zipfile
+import pandas as pd
+import urllib.parse
+import numpy as np
 
 class PyGeoCDL:
     def __init__(self, url_base=None):
@@ -50,7 +52,7 @@ class PyGeoCDL:
 
             # Call utility function to zip all auxillary shapefile files 
             # together
-            zip_file_path = str(utils.zip_shapefiles(file))
+            zip_file_path = str(self._zip_shapefiles(file))
 
             files = {"geom_file": (zip_file_path, open(zip_file_path, 'rb'))}
             r = requests.post(self.url_base + '/upload_geom', files=files)
@@ -77,12 +79,107 @@ class PyGeoCDL:
         req_name = None
     ):
         # Current status: calls helper functions and prints out results
-        dsvars_string = utils.format_dsvars(dsvars)
-        date_string = utils.format_dates(
+        dsvars_string = self._format_dsvars(dsvars)
+        date_string = self._format_dates(
             dates=dates, years=years, months=months, days=days
         )
-        geom_string = utils.format_geometry("subset_polygon", t_geom)
+        geom_string = self._format_geometry("subset_polygon", t_geom)
         print("dsvars: ", dsvars_string)
         print("dates: ", date_string)
         print("geom: ", geom_string)
+
+    # Helper function for identifying auxillary shapefile files and compressing
+    # them into a zip folder for upload
+
+    def _zip_shapefiles(self, file: str):
+
+        file = Path(file).absolute()
+
+        suffix_list = [".shp", ".shx", ".dbf", ".prj"]
+
+        # Create a new folder for shapefiles to be zipped in
+        output_zip_dir = Path(file.with_suffix(".zip"))
+
+        # Add each auxillary file, check to make sure they exist
+        with zipfile.ZipFile(output_zip_dir, "w") as z:
+            for suffix in suffix_list:
+                new_file = file.with_suffix(suffix)
+                if not new_file.is_file():
+                    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), new_file)
+                z.write(new_file)
+
+        return(output_zip_dir)
+
+    # Input ds is a pandas DataFrame, a numpy array, a matrix, or a 
+    # dictionary containing dataset names and variable names.
+    # If input is not a pandas DataFrame, convert to a DataFrame.
+    # Return the url-encoded string containing dataset and variable
+    # specifications.
+    def _format_dsvars(self, ds):
+
+        if isinstance(ds, pd.DataFrame):
+            dsvars = ds.iloc[:,0:2]
+            dsvars.columns = ["dataset", "variable"]
+        elif isinstance(ds, np.ndarray):
+            dsvars = pd.DataFrame(ds, columns=["dataset", "variable"])
+            dsvars = dsvars.iloc[:,0:2]
+        elif isinstance(ds, list):
+            dsvars = pd.DataFrame(ds, columns=["dataset", "variable"])
+            dsvars = dsvars.iloc[:,0:2]
+        elif isinstance(ds, dict):
+            dsvars = pd.DataFrame(columns=["dataset", "variable"])
+            for dataset, var_list in ds.items():
+                for var in var_list:
+                    dsvars.loc[len(dsvars.index)] = [dataset, var]
+        else:
+            raise Exception("Dataset variables format not accepted")
+
+        dsvars['variable'] = dsvars.groupby(['dataset'])['variable'].transform(lambda x : ','.join(x)) 
+        dsvars = dsvars.drop_duplicates()
+        dsvars = dsvars['dataset'].str.cat(dsvars['variable'], sep=':')
+        output = dsvars.str.cat(sep=';')
+        url_output = urllib.parse.urlparse(output)
+        return(output)
+
+    # Create string specifying dates for request
+    def _format_dates(self, dates=None, years=None, months=None, days=None):
+        temporal_subset = ""
+        if dates is not None:
+            if isinstance(dates, list):
+                temporal_subset = "&dates=" + ",".join(dates)
+            else:
+                temporal_subset = "&dates=" + dates
+        elif years is not None:
+            if isinstance(years, list):
+                temporal_subset = "&years="+ ",".join(years)
+            else:
+                temporal_subset = "&years="+ years
+            if months is not None:
+                if isinstance(months, list):
+                    temporal_subset += "&months=" + ",".join(months)
+                else:
+                    temporal_subset += "&months=" + months
+            if days is not None:
+                if isinstance(days, list):
+                    temporal_subset += "&days=" + ",".join(days)
+                else:
+                    temporal_subset += "&days=" + days
+        return(temporal_subset)
+
+    # Create string representing geometry information for request
+    def _format_geometry(self, endpoint, geom):
+        spatial_subset = ""
+        if geom is None:
+            print("Geom is None")
+            return spatial_subset
+        elif isinstance(geom, str):
+            #if geom is a guid
+            if len(geom) == 36 and not "." in geom:
+                spatial_subset += "&geom_guid=" + geom
+            else: #assume geom is a filename, attempt to upload
+                geom_guid = self.upload_geometry(geom)
+                spatial_subset += "&geom_guid=" + geom_guid
+        else: 
+            raise Exception("Geometry configuration not implemented")
+        return(spatial_subset)
         
