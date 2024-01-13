02f1d1b4b629dcca50d7961c94ada6f964a9d3d8
Noa Mills <noa.mills@usda.gov>
2024-01-12 16:00:13 -0800

    Draft of download_polygon_subsets function. Includes client example code that avoids known bugs in downloading PRISM data

--- client_example.py
+++ client_example.py
@@ -1,70 +1,92 @@
+# This script is "bug-avoidant", in that the api calls specified avoid known 
+# issues that are currently being troubleshot. Removal of the CRS parameter may
+# cause an Internal Server Error and cause the data to fail to download.
+
+# This code presents four different ways to download the same subset of PRISM
+# data, including two ways to specify the spatial data and two ways to specify
+# the temporal data. The user can confirm that the data downloaded from each
+# of the four API calls are equivalent.
+
 import sys
 import pygcdl
 import geopandas as gpd
 import os
 import pandas as pd
 import numpy as np
+from pathlib import Path
 
 pygcdl_obj = pygcdl.PyGeoCDL()
-#print(pygcdl_obj.list_datasets())
-#print(pygcdl_obj.get_dataset_info("MODIS_NDVI"))
 
-# Upload sample geometries in various file formats
+# First we request information about what datasets are available, then we
+# request more information about the PRISM dataset
 
-# Attempt to upload a geodataframe
-subset_counties1 = gpd.read_file("sample_data/subset_counties1.shp")
-subset_counties2 = gpd.read_file("sample_data/subset_counties2.shp")
-subset_counties3 = gpd.read_file("sample_data/subset_counties3.shp")
-subset_counties4 = gpd.read_file("sample_data/subset_counties4.shp")
-subset_counties5 = gpd.read_file("sample_data/subset_counties5.shp")
-subset_counties6 = gpd.read_file("sample_data/subset_counties6.shp")
-subset_counties7 = gpd.read_file("sample_data/subset_counties7.shp")
-subset_counties8 = gpd.read_file("sample_data/subset_counties8.shp")
+print(pygcdl_obj.list_datasets())
+print(pygcdl_obj.get_dataset_info("PRISM"))
 
-guid1 = pygcdl_obj.upload_geometry(subset_counties1)
-guid2 = pygcdl_obj.upload_geometry(subset_counties2)
-guid3 = pygcdl_obj.upload_geometry(subset_counties3)
-guid4 = pygcdl_obj.upload_geometry(subset_counties4)
-guid5 = pygcdl_obj.upload_geometry(subset_counties5)
-guid6 = pygcdl_obj.upload_geometry(subset_counties6)
-guid7 = pygcdl_obj.upload_geometry(subset_counties7)
-guid8 = pygcdl_obj.upload_geometry(subset_counties8)
+jer_bounds_sf = gpd.read_file("sample_data/jer_bounds_sf.shp")
+crs_jer = jer_bounds_sf.crs
 
-# Currently, download_polygon_subset just runs the helper functions
-# utils.format_dsvars(), and utils.format_dates() and returns the
-# url-encoded strings each function generates.
-
-# Create sample datasets containing dataset names and variables to use as test
-# input for utils.format_dsvars().
-
-pandas_df = pd.DataFrame([["MODIS_NDVI", "NDVI"], ["PRISM","ppt"], ["PRISM", "tmax"], ["MODIS_NDVI", "test_var"]], columns = ["dataset", "variable"])
-numpy_df = np.array([["MODIS_NDVI", "NDVI"], ["PRISM","ppt"], ["PRISM", "tmax"], ["MODIS_NDVI", "test_var"]])
-matrix_df = [["MODIS_NDVI", "NDVI"], ["PRISM","ppt"], ["PRISM", "tmax"], ["MODIS_NDVI", "test_var"]]
-dict_df = {"MODIS_NDVI":["NDVI", "test_var"], "PRISM":["ppt","tmax"]}
+# Here we create guids from the same data in two ways.
+# First, we upload a file, then we upload a geodataframe.
+guid0 = pygcdl_obj.upload_geometry("sample_data/jer_bounds_sf.shp")
+guid1 = pygcdl_obj.upload_geometry(jer_bounds_sf)
 
 
-# Create sample date data to use for utils.format_dates(). This function can
-# accept datestrings, years, months, and days as either strings separated by
-# commas, or as lists of strings.
+prism_df = pd.DataFrame([["PRISM", "ppt"]], columns =
+    ["dataset", "variable"])
 
-dates1 = "2000-01-01:2000-02-01,2001-01-01:2001-02-01"
-dates2 = ["2000:2001", "2003"]
-years = "2000,2003:2004"
-months = ["1", "6", "9"]
-days = ["100:200"]
+dates = "2000-01-01:2000-01-03"
+years = "2000"
+months = "01"
+days = "01:03"
 
-# Here we apply the download_polygon_subset function on various dataset/variable
-# data, date data, and geometry data
+# Create output directories to store downloaded data
+output1 = Path("output1")
+output2 = Path("output2")
+output3 = Path("output3")
+output4 = Path("output4")
+
+output_dirs = [output1, output2, output3, output4]
+for out in output_dirs:
+    if not out.is_dir():
+        out.mkdir()
 
 print("Call 1:")
-pygcdl_obj.download_polygon_subset(pandas_df, dates=dates1, \
-    t_geom = guid1)
+pygcdl_obj.download_polygon_subset(
+    prism_df, 
+    dates=dates, 
+    t_geom=guid0,
+    dsn=output1,
+    t_crs=crs_jer
+    )
+
 print("Call 2:")
-pygcdl_obj.download_polygon_subset(matrix_df, dates=dates2, \
-    t_geom = guid2)
+pygcdl_obj.download_polygon_subset(
+    prism_df, 
+    years=years,
+    months=months,
+    days=days, 
+    t_geom=guid0,
+    dsn=output2,
+    t_crs=crs_jer
+    )
+
 print("Call 3:")
-pygcdl_obj.download_polygon_subset(dict_df, years=years, months=months, \
-    t_geom = "sample_data/subset_counties4.shp")
+pygcdl_obj.download_polygon_subset(
+    prism_df,
+    dates=dates,
+    t_geom=guid1,
+    dsn=output3,
+    t_crs=crs_jer
+    )
+
 print("Call 4:")
-pygcdl_obj.download_polygon_subset(numpy_df, years=years, days=days, \
-    t_geom = "sample_data/clay_aoi.geojson")
+pygcdl_obj.download_polygon_subset(
+    prism_df, 
+    years=years,
+    months=months,
+    days=days, 
+    t_geom=guid1,
+    dsn=output4,
+    t_crs=crs_jer
+    )
--- pygcdl.py
+++ pygcdl.py
@@ -8,6 +8,7 @@ import urllib.parse
 import numpy as np
 import geopandas as gpd
 import shapely
+import datetime
 
 class PyGeoCDL:
     def __init__(self, url_base=None):
@@ -120,15 +121,130 @@ class PyGeoCDL:
         dsn = '.',
         req_name = None
     ):
-        # Current status: calls helper functions and prints out results
-        dsvars_string = self._format_dsvars(dsvars)
-        date_string = self._format_dates(
-            dates=dates, years=years, months=months, days=days
+
+        endpoint = 'subset_polygon'
+
+        q_str, param_dict = self._format_subset_query(
+            dsvars=dsvars, 
+            endpoint=endpoint,
+            dates=dates, 
+            years=years, 
+            months=months, 
+            days=days, 
+            t_crs=t_crs, 
+            resolution=resolution, 
+            t_geom=t_geom,
+            out_format=out_format, 
+            grain_method=grain_method,
+            validate_method=validate_method,
+            ri_method=ri_method)
+
+        out_files = self._submit_subset_query(q_str, param_dict, dsn, req_name)
+        return out_files
+
+    def _format_subset_query(
+        self,
+        dsvars,
+        endpoint,
+        dates=None,
+        years=None,
+        months=None,
+        days=None,
+        t_crs=None, 
+        resolution=None,
+        t_geom=None,
+        out_format='geotiff',
+        grain_method='strict',
+        validate_method='strict',
+        ri_method='nearest',
+    ):
+
+        get_params_dict = {}
+
+         # Format datasets and variables into string
+        dv_str = self._format_dsvars(dsvars)
+        get_params_dict.update(dv_str)
+
+        # Format user geometry
+        spatial_subset = self._format_geometry(endpoint, t_geom)
+        get_params_dict.update(spatial_subset)
+
+        # Format spatial parameters (crs, resolution, interpolation/resampling method)
+        spatial_parameters = self._format_spatial_parameters(
+            endpoint, 
+            ri_method, 
+            t_crs, 
+            resolution
         )
-        geom_string = self._format_geometry("subset_polygon", t_geom)
-        print("dsvars: ", dsvars_string)
-        print("dates: ", date_string)
-        print("geom: ", geom_string)
+        get_params_dict.update(spatial_parameters)
+
+        # Format temporal subset instructions
+        temporal_subset = self._format_dates(dates, years, months, days)
+        get_params_dict.update(temporal_subset)
+
+        # Temporal parameters (grain method, validate method)
+        temporal_parameters = self._format_temporal_parameters(
+            grain_method, 
+            validate_method
+        )
+        get_params_dict.update(temporal_parameters)
+
+        # Other parameters
+        other_parameters = {}
+        if out_format is not None:
+            other_parameters['output_format'] = out_format
+        get_params_dict.update(other_parameters)
+        
+        
+        # Build query
+        query_str = self.url_base + "/" + endpoint
+
+        return query_str, get_params_dict
+
+    def _submit_subset_query(self, query_str, param_dict, dsn, req_name):
+        # dsn = path to directory
+        # req_name = name of folder to put in dsn, with downloaded data
+        if not Path(dsn).is_dir():
+            raise Exception("Destination folder DNE")
+
+        # Create output directory
+        if req_name is None:
+            basename = "gcdl_subset"
+            suffix = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
+            output_dir = "_".join([basename, suffix])
+        
+        spatial_params = param_dict["geom_guid"]
+        if isinstance(spatial_params, str):
+            param_dict["geom_guid"] = [spatial_params]
+            num_queries = 1
+        elif isinstance(spatial_params, list):
+            num_queries = len(spatial_params)
+        else:
+            raise Exception("No query provided")
+            return
+
+        out_files = []
+        headers = {"accept": "application/json"}
+
+        for q in range(num_queries):
+            out = output_dir + "_" + str(q+1)
+            subset_dir = Path(dsn) / Path(out)
+            subset_zip = Path(str(subset_dir) + ".zip")
+            req_spatial = param_dict["geom_guid"][q]
+            params = param_dict.copy()
+            params["geom_guid"] = req_spatial
+            r = requests.get(query_str, params=params, headers=headers)
+            print(r.url)
+            if r.status_code >= 400:
+                print("Status_code: ", r.status_code)
+                print(r.text)
+            else:
+                with open(subset_zip, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=8192): 
+                        f.write(chunk)
+                    print("Written file: ", str(subset_zip))
+
+        return subset_zip
 
     # Helper function for identifying auxillary shapefile files and compressing
     # them into a zip folder for upload
@@ -157,20 +273,20 @@ class PyGeoCDL:
     # If input is not a pandas DataFrame, convert to a DataFrame.
     # Return the url-encoded string containing dataset and variable
     # specifications.
-    def _format_dsvars(self, ds):
+    def _format_dsvars(self, dsvars):
 
-        if isinstance(ds, pd.DataFrame):
-            dsvars = ds.iloc[:,0:2]
+        if isinstance(dsvars, pd.DataFrame):
+            dsvars = dsvars.iloc[:,0:2]
             dsvars.columns = ["dataset", "variable"]
-        elif isinstance(ds, np.ndarray):
-            dsvars = pd.DataFrame(ds, columns=["dataset", "variable"])
+        elif isinstance(dsvars, np.ndarray):
+            dsvars = pd.DataFrame(dsvars, columns=["dataset", "variable"])
             dsvars = dsvars.iloc[:,0:2]
-        elif isinstance(ds, list):
-            dsvars = pd.DataFrame(ds, columns=["dataset", "variable"])
+        elif isinstance(dsvars, list):
+            dsvars = pd.DataFrame(dsvars, columns=["dataset", "variable"])
             dsvars = dsvars.iloc[:,0:2]
-        elif isinstance(ds, dict):
+        elif isinstance(dsvars, dict):
             dsvars = pd.DataFrame(columns=["dataset", "variable"])
-            for dataset, var_list in ds.items():
+            for dataset, var_list in dsvars.items():
                 for var in var_list:
                     dsvars.loc[len(dsvars.index)] = [dataset, var]
         else:
@@ -180,53 +296,83 @@ class PyGeoCDL:
         dsvars = dsvars.drop_duplicates()
         dsvars = dsvars['dataset'].str.cat(dsvars['variable'], sep=':')
         output = dsvars.str.cat(sep=';')
-        url_output = urllib.parse.urlparse(output)
-        return(output)
+        dsvars_str = {"datasets": output}
+        return(dsvars_str)
 
     # Create string specifying dates for request
     def _format_dates(self, dates=None, years=None, months=None, days=None):
-        temporal_subset = ""
+        temporal_subset = {}
         if dates is not None:
             if isinstance(dates, list):
-                temporal_subset = "&dates=" + ",".join(dates)
+                temporal_subset["dates"] = ",".join(dates)
             else:
-                temporal_subset = "&dates=" + dates
+                temporal_subset["dates"] = dates
         elif years is not None:
             if isinstance(years, list):
-                temporal_subset = "&years="+ ",".join(years)
+                temporal_subset["years"] = ",".join(years)
             else:
-                temporal_subset = "&years="+ years
+                temporal_subset["years"] = years
             if months is not None:
                 if isinstance(months, list):
-                    temporal_subset += "&months=" + ",".join(months)
+                    temporal_subset["months"] = ",".join(months)
                 else:
-                    temporal_subset += "&months=" + months
+                    temporal_subset["months"] = months
             if days is not None:
                 if isinstance(days, list):
-                    temporal_subset += "&days=" + ",".join(days)
+                    temporal_subset["days"] = ",".join(days)
                 else:
-                    temporal_subset += "&days=" + days
+                    temporal_subset["days"] = days
         return(temporal_subset)
 
     # Create string representing geometry information for request
     def _format_geometry(self, endpoint, geom):
-        spatial_subset = ""
+        spatial_subset = {}
         if geom is None:
-            print("Geom is None")
+            print("No geometry specified")
             return spatial_subset
         elif isinstance(geom, str):
-            #if geom is a guid
-
+            #if geom is a single guid
             if len(geom) == 36 and not "." in geom:
-                spatial_subset += "&geom_guid=" + geom
+                spatial_subset["geom_guid"] = geom
             else: #assume geom is a filename, attempt to upload
                 geom_guid = self.upload_geometry(geom)
-                spatial_subset += "&geom_guid=" + geom_guid
+                spatial_subset["geom_guid"] = geom_guid
         elif isinstance(geom, list) and \
             all(len(geom[i]) == 36 for i in range(len(geom))):
+            guid_list = []
             for i in range(len(geom)):
-                spatial_subset += "&geom_guid" + geom[i]
+                guid_list.append(geom[i])
+            spatial_subset["geom_guid"] = guid_list
         else: 
             raise Exception("Geometry configuration not implemented")
         return(spatial_subset)
+
+    def _format_temporal_parameters(self, grain_method, validate_method):
+        temporal_parameters = {
+            "grain_method": grain_method,
+            "validate_method": validate_method
+        }
+        return(temporal_parameters)
+
+    def _format_spatial_parameters(
+        self, 
+        endpoint, 
+        ri_method, 
+        t_crs, 
+        resolution
+    ):
+        spatial_parameters = {}
+        if endpoint == "subset_points":
+            spatial_parameters["interpolation_method"] = ri_method
+        else:
+            spatial_parameters["resample_method"] = ri_method
+        
+        if t_crs is not None:
+            spatial_parameters["crs"] = str(t_crs)
+
+        if resolution is not None:
+            spatial_parameters["resolution"] = str(resolution)
+
+        return spatial_parameters
+
         
